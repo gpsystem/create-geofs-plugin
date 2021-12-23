a53e802bcda2d36f847e0c365cf90c6c1d747c1e
nicolas377 <programnicolas@gmail.com>
2021-12-23 17:05:52 -0500

    got rid of unneeded edge case
    the template is built with it anyways, no need to return null if it doesnt exist

--- packages/template-typescript/src/getTemplateZip.ts
+++ packages/template-typescript/src/getTemplateZip.ts
@@ -1,14 +1,11 @@
-import { existsSync, readFileSync } from "fs";
+import { readFileSync } from "fs";
 import { join } from "path";
 
 /**
  * Gets the template.
- * @returns null if there is no template, otherwise, returns a Buffer of the template.zip
+ * @returns a Buffer of the template.zip
  */
-export function getTemplateZip(): Buffer | null {
+export function getTemplateZip(): Buffer {
   const pathToTemplate = join(__dirname, "template.zip");
-  if (!existsSync(pathToTemplate)) {
-    return null;
-  }
   return readFileSync(pathToTemplate);
 }

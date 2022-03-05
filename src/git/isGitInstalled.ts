df132eaf23a848b800fb726f1843702baa201dcb
nicolas377 <programnicolas@gmail.com>
2022-03-05 15:25:54 -0500

    refactor(git): used a simpler implementation for isGitInstalled()
    using execFile provides more stability
    
    fixed return statement to resolution
    
    closes #64

--- src/git/isGitInstalled.ts
+++ src/git/isGitInstalled.ts
@@ -1,12 +1,13 @@
-import { exec } from "node:child_process";
+import { execFile } from "node:child_process";
 import { yellow } from "../chalkTypes";
 
 export default function isGitInstalled(): Promise<boolean> {
-  return new Promise<boolean>((resolve) => {
-    exec("git --version", (err) => {
+  return new Promise((resolve) => {
+    // execFile allows us to give the args as a string[]
+    execFile("git", ["--version"], (err) => {
       if (err) {
         console.log(yellow("`git` binary not found"));
-        return false;
+        resolve(false);
       } else resolve(true);
     });
   });

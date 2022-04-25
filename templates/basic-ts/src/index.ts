a0dd9319d5075b84af4c0678f2efe465add5965a
Nicolas Rodriguez <programnicolas@gmail.com>
2022-04-25 19:10:28 -0400

    feat(templates): :sparkles: create basic-ts template (#127)

---
+++ templates/basic-ts/src/index.ts
@@ -0,0 +1,3 @@
+// start writing your code here!
+// you can reference the geofs apis in intellisense because they were included in the tsconfig
+// the apis from geofs plugin system are not available yet

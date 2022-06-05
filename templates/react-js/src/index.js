88032ff3feeae65302c3b9bb47a958ae1f6029ae
nicolas377 <programnicolas@gmail.com>
2022-06-05 10:33:53 -0400

    chore(temp-test): :beers: add a test to react-js' source
    i'll remove this later, this is just to make sure compilation works with babel, react, and webpack

--- templates/react-js/src/index.js
+++ templates/react-js/src/index.js
@@ -1,6 +1,20 @@
 /// <reference types="jquery" />
 /// <reference types="@geps/geofs-types" />
 
+import { createRoot } from "react-dom/client";
+import React from "react";
+
 // start writing your code here!
 // you can reference the geofs apis in intellisense because of the triple-slash directives
 // the apis from geofs plugin system are not available yet
+
+function Main() {
+  return (
+    <React.StrictMode>
+      <h1>Test</h1>
+    </React.StrictMode>
+  );
+}
+
+const root = createRoot(document.getElementById("test"));
+root.render(<Main />);

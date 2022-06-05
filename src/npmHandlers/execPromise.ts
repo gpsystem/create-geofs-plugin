56026fee144997589c4984cc178d2fe8725db0a4
nicolas377 <programnicolas@gmail.com>
2022-06-04 20:47:45 -0400

    refactor(execPromise): :recycle: use promisified child_process.exec
    storing the result of util.promisify in a variable will also help with the very small performance gains

--- src/npmHandlers/execPromise.ts
+++ src/npmHandlers/execPromise.ts
@@ -1,19 +1,16 @@
 import { exec } from "node:child_process";
+import { promisify } from "node:util";
 
-// TODO: should we replace this with util.promisify(exec)?
+const promisifiedExec = promisify(exec);
+
 /**
  * A promisified child_process.{@link exec}.
  * @param command The command to run in the shell. Not sanitized before passed to child_process.exec.
  * @param workingDir The directory to run the command in.
  */
-export default function execPromise(
+export default async function execPromise(
   command: string,
   workingDir: string
 ): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    exec(command, { cwd: workingDir }, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
+  await promisifiedExec(command, { cwd: workingDir });
 }

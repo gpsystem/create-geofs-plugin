516c337b2d2069c110ab636373836f3f9a0e8882
nicolas377 <programnicolas@gmail.com>
2022-06-08 15:46:49 -0400

    chore: random small stuff

--- packages/create-geofs-plugin/index.js
+++ packages/create-geofs-plugin/index.js
@@ -1,2 +1,44 @@
 #!/usr/bin/env node
-require("./bin/create-geofs-plugin").start(process.argv);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// create-geofs-plugin is installed globally on people's computers. This means
+// that it is extremely difficult to have them upgrade the version and
+// because there's only one global version installed, it is very prone to
+// breaking changes.
+//
+// The only reason to modify this file is to add more warnings and
+// troubleshooting information for the `create-react-app` command.
+//
+// Do not make breaking changes! We absolutely don't want to have to
+// tell people to update their global version of create-react-app.
+//
+// Also be careful with new language features.
+// This file must work on Node 0.10+.
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"use strict";
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split(".");
+const major = semver[0];
+
+if (Number(major) < 14) {
+  console.error(
+    "You are running Node " +
+      currentNodeVersion +
+      ".\n" +
+      "Create GeoFS Plugin requires Node 14 or higher. \n" +
+      "Please update your version of Node."
+  );
+  process.exit(1);
+}
+
+const { start } = require("./bin/create-geofs-plugin");
+
+start();
